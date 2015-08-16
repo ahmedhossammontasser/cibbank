a73cec1ae4117c802be46e01dc0f0a0cd236961f
ahmedhossammontasser <a.hossam.2010@gmail.com>
2015-08-16 12:54:10 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cibbanktask::Application.config.secret_token = 'fc7116b1d4110fc09b89adbec7d04e45890e1d6d11e7539282d914e54c5c857a97c7698edf9d116fce8db3c08bbed9db4e072a1f6a299ab26fb6a35e90467b7b'
