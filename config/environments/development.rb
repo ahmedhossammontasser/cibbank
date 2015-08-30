0eb90a5524eadf6be9078368b6cec13370f3eaf7
ahmedhossammontasser <a.hossam.2010@gmail.com>
2015-08-30 21:09:08 +0200

    Test email 1

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,6 +18,8 @@ Cibbanktask::Application.configure do
   config.action_mailer.default :charset => "utf-8"
 
   # Don't care if the mailer can't send
+  config.action_mailer.raise_delivery_errors = false
+config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
 
   # Print deprecation notices to the Rails logger
